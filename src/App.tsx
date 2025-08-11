2d3da2298042d92f9a5540c34288e36af8cece2f
UmiduKaushika97 <umidu.kaushika@gmail.com>
2025-08-11 20:08:45 +0530

    for testing2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // import './App.css'
 
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/AppRouter.tsx'
+
 
 function App() {
  
 
   return (
    <>
-   
+    <RouterProvider router={router} />
+    {/* i added this after beacuse of build file  */}
    </>
   )
 }
